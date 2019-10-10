5e48072da839bb7f2713f2e8a4cc08c96d9f13e9
nesnip <hpmarines12@gmail.com>
2019-10-10 08:48:56 -0500

    corrección de cifrado para todas las letras y de style

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,66 @@
 window.cipher = {
   // Acá escribe tu código.
+  encode: (offset, string) => {
+    /* Acá va tu código que cifra*/
+    let cipEncode = "";
+    for (let i = 0; i < string.length; i++) {
+      let txt = string.charCodeAt(i);
+      if (65 <= txt && txt <= 90) {
+        cipEncode += String.fromCharCode(
+          ((txt - 65 + parseInt(offset)) % 26) + 65
+        );
+      } else if (97 <= txt && txt <= 122) { 
+        cipEncode += String.fromCharCode(
+          ((txt - 97 + parseInt(offset)) % 26) + 97
+        );
+      } else {
+        cipEncode += string.charAt(i);
+      }
+    }
+    return cipEncode;
+
+    /*let result = "";
+      //inicio de recorrido de caracteres
+      for (let i = 0; i < string.length; i++) {
+        let asciiPosition = string[i];
+        if (asciiPosition.match(/[a-z]/i)) {
+
+          if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+            let text = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+            result += String.fromCharCode(text);
+          }
+          else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+            text = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
+            result += String.fromCharCode(text);
+          }
+        } 
+        else {
+          result += asciiPosition;
+        }
+      }
+      return result;*/
+  },
+
+  decode: (offset, string) => {
+    /* Acá va tu código que descifra*/
+    let cipDecode = "";
+
+    for (let i = 0; i < string.length; i++) {
+      let txt = string.charCodeAt(i);
+      if (65 <= txt && txt <= 90) {
+        cipDecode += String.fromCharCode(
+          ((txt + 65 - parseInt(offset)) % 26) + 65
+        );
+      } else if (97 <= txt && txt <= 122) {
+        cipDecode += String.fromCharCode(
+          ((txt - 97 - parseInt(offset) + 52) % 26) + 97
+        );
+      } else {
+        cipDecode += string.charAt(i);
+      }
+    }
+    return cipDecode;
+  },
+
 };
+
--- src/index.js
+++ src/index.js
@@ -4,18 +4,53 @@ const root = document.getElementById('root');
 const options = document.getElementById('options');
 const cifrado = document.getElementById('cifrado');
 const cifResult = document.getElementById('cifResult');
+const descifrado = document.getElementById('descifrado');
+const descifResult = document.getElementById('descifResult');
+const encriptar = document.getElementById('encriptar');
+const desencriptar = document.getElementById('desencriptar');
 
-aceptar.addEventListener('click', ()=>{
+root.classList.remove('hide');
+
+aceptar.addEventListener('click', () => {
     root.classList.add('hide');
     options.classList.remove('hide');
-})
+});
 
-cifrar.addEventListener('click', ()=>{
+cifrar.addEventListener('click', () => {
     options.classList.add('hide');
     cifrado.classList.remove('hide');
-})
+});
 
-code.addEventListener('click', ()=>{
+encriptar.addEventListener('click', () => {
     cifrado.classList.add('hide');
     cifResult.classList.remove('hide');
-})
+    let strNom = document.getElementById('nombre');
+    let string = document.getElementById('texto').value;
+    let offset = document.getElementById('desplazamiento').value;
+    window.cipher.encode(offset, string, nombre);
+    document.getElementById('textoCifrado').innerHTML = window.cipher.encode(offset, string);
+});
+
+descifrar.addEventListener('click', () => {
+    options.classList.add('hide');
+    descifrado.classList.remove('hide');
+});
+
+desencriptar.addEventListener('click', () => {
+    descifrado.classList.add('hide');
+    descifResult.classList.remove('hide');
+    let string = document.getElementById('texto2').value;
+    let offset = document.getElementById('desplazamiento2').value;
+    window.cipher.decode(offset, string);
+    document.getElementById('textoDescifrado').innerHTML = window.cipher.decode(offset, string);
+});
+
+inicio1.addEventListener('click', () => {
+    cifResult.classList.add('hide');
+    root.classList.remove('hide');
+});
+
+inicio2.addEventListener('click', ()=>{
+    descifResult.classList.add('hide');
+    root.classList.remove('hide');
+});
