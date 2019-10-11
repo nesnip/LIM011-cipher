12f40ed1b77f498b377e78ed6088e15b2899c045
nesnip <hpmarines12@gmail.com>
2019-10-11 00:34:17 -0500

    Cambios html: eliminar botones copiar y id inutiles, css: agregar estilo a los inputs y textarea, js: eliminar comentarios y espacios vacios

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,34 @@
 window.cipher = {
   // Acá escribe tu código.
+  encode: (offset, string) => {
+    /* Acá va tu código que cifra*/
+    let cipEncode = "";
+    for (let i = 0; i < string.length; i++) {
+      let txt = string.charCodeAt(i);
+      if (65 <= txt && txt <= 90) {
+        cipEncode += String.fromCharCode(((txt - 65 + parseInt(offset)) % 26) + 65);        
+      } else if (97 <= txt && txt <= 122) { 
+        cipEncode += String.fromCharCode(((txt - 97 + parseInt(offset)) % 26) + 97);
+      } else {
+        cipEncode += string.charAt(i);
+      }
+    }
+    return cipEncode;
+  },
+
+  decode: (offset, string) => {
+    /* Acá va tu código que descifra*/
+    let cipDecode = "";
+    for (let i = 0; i < string.length; i++) {
+      let txt = string.charCodeAt(i);
+      if (65 <= txt) {
+        cipDecode += String.fromCharCode(((txt - 65 - parseInt(offset)) % 26) + 65);
+      } else if (97 <= txt && txt <= 122) {
+        cipDecode += String.fromCharCode(((txt - 97 - parseInt(offset) + 52) % 26) + 97);
+      } else {
+        cipDecode += string.charAt(i);
+      }
+    }
+    return cipDecode;
+  },
 };
---
+++ src/index.js
@@ -0,0 +1,58 @@
+const aceptar = document.getElementById('aceptar');
+const root = document.getElementById('root');
+const options = document.getElementById('options');
+const cifrado = document.getElementById('cifrado');
+const cifResult = document.getElementById('cifResult');
+const descifrado = document.getElementById('descifrado');
+const descifResult = document.getElementById('descifResult');
+const encriptar = document.getElementById('encriptar');
+const desencriptar = document.getElementById('desencriptar');
+const cifrar = document.getElementById('cifrar');
+const descifrar = document.getElementById('descifrar');
+const inicio1 = document.getElementById('inicio1');
+const inicio2 = document.getElementById('inicio2');
+
+root.classList.remove('hide');
+
+aceptar.addEventListener('click', () => {
+    root.classList.add('hide');
+    options.classList.remove('hide');
+});
+
+cifrar.addEventListener('click', () => {
+    options.classList.add('hide');
+    cifrado.classList.remove('hide');
+});
+
+encriptar.addEventListener('click', () => {
+    cifrado.classList.add('hide');
+    cifResult.classList.remove('hide');
+    let string = document.getElementById('texto').value;
+    let offset = document.getElementById('desplazamiento').value;
+    window.cipher.encode(offset, string);
+    document.getElementById('textoCifrado').innerHTML = window.cipher.encode(offset, string);
+});
+
+descifrar.addEventListener('click', () => {
+    options.classList.add('hide');
+    descifrado.classList.remove('hide');
+});
+
+desencriptar.addEventListener('click', () => {
+    descifrado.classList.add('hide');
+    descifResult.classList.remove('hide');
+    let string = document.getElementById('texto2').value;
+    let offset = document.getElementById('desplazamiento2').value;
+    window.cipher.decode(offset, string);
+    document.getElementById('textoDescifrado').innerHTML = window.cipher.decode(offset, string);
+});
+
+inicio1.addEventListener('click', () => {
+    cifResult.classList.add('hide');
+    root.classList.remove('hide');
+});
+
+inicio2.addEventListener('click', ()=>{
+    descifResult.classList.add('hide');
+    root.classList.remove('hide');
+});
