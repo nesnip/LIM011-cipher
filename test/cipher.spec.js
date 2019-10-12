bcec3f17591a7e43f503dfdb67a6fe7ef8574a0b
nesnip <hpmarines12@gmail.com>
2019-10-11 22:14:36 -0500

    eliminé comentarios sobrantes de cipher.spec.js

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -19,26 +19,6 @@ describe('cipher', () => {
       expect(cipher.encode(33, ' !@')).toBe(' !@');
     });
 
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides agregar soporte para minúsculas descomenta el test a
-    // continuación.
-    //
-    // it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
-    //   expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
-    // });
-
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides implementar soporte para caracteres no alfabéticos descomenta
-    // el test a continuación.
-    //
-    //
-    // it('debería retornar " !@" para " !@"', () => {
-    //   expect(cipher.encode(33, ' !@')).toBe(' !@');
-    // });
   })
   describe('cipher.decode', () => {
     it('debería ser una función', () => {
@@ -53,27 +33,5 @@ describe('cipher', () => {
     it('debería retornar " !@" para " !@"', () => {
       expect(cipher.decode(33, ' !@')).toBe(' !@');
     });
-    //
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides agregar soporte para minúsculas descomenta el test a
-    // continuación.
-    //
-    //
-    // it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
-    //   expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
-    // });
-
-    // Hacker edition
-    //
-    // [Español]
-    // Si decides implementar soporte para caracteres no alfabéticos descomenta
-    // el test a continuación.
-    //
-    //
-    // it('debería retornar " !@" para " !@"', () => {
-    //   expect(cipher.decode(33, ' !@')).toBe(' !@');
-    // });
   });
 });
